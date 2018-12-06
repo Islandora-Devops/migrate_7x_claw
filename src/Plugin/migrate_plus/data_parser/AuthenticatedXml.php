e103540660d80f36309e82804542604a124a925a
dannylamb <dlamb@islandora.ca>
2018-12-06 20:06:45 +0000

    Now working with AUDIT datastreams

--- src/Plugin/migrate_plus/data_parser/AuthenticatedXml.php
+++ src/Plugin/migrate_plus/data_parser/AuthenticatedXml.php
@@ -33,23 +33,31 @@ class AuthenticatedXml extends SimpleXml {
   /**
    * {@inheritdoc}
    */
-  protected function openSourceUrl($url) {
-    // Clear XML error buffer. Other Drupal code that executed during the
-    // migration may have polluted the error buffer and could create false
-    // positives in our error check below. We are only concerned with errors
-    // that occur from attempting to load the XML string into an object here.
-    libxml_clear_errors();
-
-    $xml_data = $this->getDataFetcherPlugin()->getResponseContent($url)->getContents();
-    $xml = simplexml_load_string($xml_data);
-    $this->registerNamespaces($xml);
-    $xpath = $this->configuration['item_selector'];
-    $this->matches = $xml->xpath($xpath);
-    foreach (libxml_get_errors() as $error) {
-      $error_string = self::parseLibXmlError($error);
-      throw new MigrateException($error_string);
+  protected function fetchNextRow() {
+    $target_element = array_shift($this->matches);
+    // If we've found the desired element, populate the currentItem and
+    // currentId with its data.
+    if ($target_element !== FALSE && !is_null($target_element)) {
+      foreach ($this->fieldSelectors() as $field_name => $xpath) {
+        foreach ($target_element->xpath($xpath) as $value) {
+          if ($value->children() && !trim((string) $value)) {
+            $this->currentItem[$field_name] = $value;
+          }
+          elseif (!trim((string) $value)){
+            $this->currentItem[$field_name][] = $value->asXML();
+          }
+          else {
+            $this->currentItem[$field_name][] = (string) $value;
+          }
+        }
+      }
+      // Reduce single-value results to scalars.
+      foreach ($this->currentItem as $field_name => $values) {
+        if (count($values) == 1) {
+          $this->currentItem[$field_name] = reset($values);
+        }
+      }
     }
-    return TRUE;
   }
 
   /**
